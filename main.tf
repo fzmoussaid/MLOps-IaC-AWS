71c9d154b9c4bf75c0797733f1012ba39303916b
fzmoussaid <fatima.zahra.moussaid94@gmail.com>
2024-08-31 23:25:44 +0900

    Deploy own model from ecr image

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
@@ -64,56 +64,54 @@ resource "aws_lambda_function" "inference_lambda" {
   }
 }
 
+resource "aws_sagemaker_endpoint" "inference_endpoint" {
+  name                 = "inference-endpoint"
+  endpoint_config_name = aws_sagemaker_endpoint_configuration.inference_endpoint_config.name
 
-# ## Inference endpoint
-# resource "aws_sagemaker_endpoint" "inference_endpoint" {
-#   name                 = "inference-endpoint"
-#   endpoint_config_name = aws_sagemaker_endpoint_configuration.inference_endpoint_config.name
-
-#   tags = {
-#     Name = "dev-endpoint"
-#   }
-# }
-
-# resource "aws_sagemaker_endpoint_configuration" "inference_endpoint_config" {
-#   name = "inference-endpoint-config"
-
-#   production_variants {
-#     variant_name           = "DevVariant"
-#     model_name             = "kmeansTestModel"
-
-#     serverless_config {
-#       max_concurrency = 10
-#       memory_size_in_mb = 3072
-#     }
-#   }
-
-#   tags = {
-#     Name = "dev-endpoint-config"
-#   }
-# }
-
-# todo: add model
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sagemaker_model
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/sagemaker_prebuilt_ecr_image
-
-
-## Sagemaker Model
-resource "aws_sagemaker_model" "kmeansTestModel" {
-  name               = "kmeansTestModel"
-  execution_role_arn = aws_iam_role.modelIamRole.arn
-
-  primary_container {
-    image = data.aws_sagemaker_prebuilt_ecr_image.kmeansEcrImage.registry_path
+  tags = {
+    Name = "dev-endpoint"
   }
 }
 
-resource "aws_iam_role" "modelIamRole" {
-  assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
+resource "aws_sagemaker_endpoint_configuration" "inference_endpoint_config" {
+  name = "inference-endpoint-config"
+
+  production_variants {
+    variant_name = "DevVariant"
+    model_name   = "vegetables-classification-model"
+
+    serverless_config {
+      max_concurrency   = 5
+      memory_size_in_mb = 3072
+    }
+  }
+
+  tags = {
+    Name = "dev-endpoint-config"
+  }
 }
 
-data "aws_iam_policy_document" "sagemaker_assume_role" {
+resource "aws_sagemaker_model" "vegetables_classification_model" {
+  name               = "vegetables-classification-model"
+  execution_role_arn = aws_iam_role.modelIamRole.arn
+
+  primary_container {
+    image = format("%s%s", var.account_id, ".dkr.ecr.us-east-1.amazonaws.com/classifier-sagemaker-deployment:latest")
+  }
+}
+
+
+resource "aws_iam_role" "modelIamRole" {
+  name               = "model-deploy-access-role"
+  assume_role_policy = data.aws_iam_policy_document.sagemaker_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.modelIamRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
+
+data "aws_iam_policy_document" "sagemaker_role" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -121,9 +119,6 @@ data "aws_iam_policy_document" "sagemaker_assume_role" {
       type        = "Service"
       identifiers = ["sagemaker.amazonaws.com"]
     }
-  }
-}
 
-data "aws_sagemaker_prebuilt_ecr_image" "kmeansEcrImage" {
-  repository_name = "kmeans"
+  }
 }
